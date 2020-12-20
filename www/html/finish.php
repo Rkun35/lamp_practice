c9d609105bf29a4445f4acf88335930fa4a99813
ryusunohara <sunosuno0035@gmail.com>
2020-12-20 09:50:47 +0900

    データ保存

--- www/html/finish.php
+++ www/html/finish.php
@@ -4,6 +4,7 @@ require_once MODEL_PATH . 'functions.php';
 require_once MODEL_PATH . 'user.php';
 require_once MODEL_PATH . 'item.php';
 require_once MODEL_PATH . 'cart.php';
+require_once MODEL_PATH . 'purchase_history.php';
 
 session_start();
 
@@ -23,12 +24,38 @@ $user = get_login_user($db);
 
 $carts = get_user_carts($db, $user['user_id']);
 
+$db->beginTransaction();
+
 if(purchase_carts($db, $carts) === false){
   set_error('商品が購入できませんでした。');
+  $db->rollback();
   redirect_to(CART_URL);
 } 
 
-
 $total_price = sum_carts($carts);
+
+//購入履歴テーブルの登録 total,user_idを渡す
+  
+ if(history_regist($total_price, $user['user_id'], $db) === false){
+   set_error('購入履歴が登録できませんでした。');
+   $db->rollback();
+   redirect_to(CART_URL);
+
+ }
+ 
+ $purchase_id = (int) $db->lastInsertId(); 
+
+foreach($carts as $cart) {
+  //購入明細テーブルの登録 item_id,price,amount
+  if(details_regist((int)$cart['item_id'], (int)$cart['price'], (int)$cart['amount'], $purchase_id, $db) === false){
+    set_error('購入履歴が登録できませんでした。');
+    $db->rollback();
+    redirect_to(CART_URL);
+ 
+  }
+
+  }
+  $db->commit();
+
 
 include_once '../view/finish_view.php';
